feae2e3dd47830db49cf9976fa4269605d43ea16
Maklakov Sergey <maklakov.sergey@hotmail.com>
2019-04-18 11:48:11 +0500

    добавлена вторая активити с кнопками и наборы цветов 3 тем

---
+++ app/src/main/java/com/mss/imaginarium/ThemeActivity.kt
@@ -0,0 +1,12 @@
+package com.mss.imaginarium
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ThemeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme)
+    }
+}
