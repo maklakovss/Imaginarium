82b59ee1704edce98b994c04c11a69c671d8c8e5
Maklakov Sergey <maklakov.sergey@hotmail.com>
2019-05-01 11:06:41 +0500

    добавлена Activity

--- app/src/main/java/com/mss/imaginarium/CollapsingActivity.kt
+++ app/src/main/java/com/mss/imaginarium/CollapsingActivity.kt
@@ -2,11 +2,15 @@ package com.mss.imaginarium
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 
 class CollapsingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collapsing)
+        findViewById<Toolbar>(R.id.toolBar).let {
+            setSupportActionBar(it)
+        }
     }
 }
