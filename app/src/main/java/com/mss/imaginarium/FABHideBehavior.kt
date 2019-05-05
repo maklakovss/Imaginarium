43daffb6388dfbcd02f3362d343cf92723df085c
Maklakov Sergey <maklakov.sergey@hotmail.com>
2019-05-05 08:33:35 +0500

    добавлено скрытие FAB при скроле

---
+++ app/src/main/java/com/mss/imaginarium/FABHideBehavior.kt
@@ -0,0 +1,41 @@
+package com.mss.imaginarium
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.view.ViewCompat
+import android.util.AttributeSet
+import android.view.View
+
+class FABHideBehavior(context: Context, attrSet: AttributeSet) : FloatingActionButton.Behavior(context, attrSet) {
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int
+    ) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type)
+
+        if (child.visibility == View.VISIBLE && dyConsumed > 0) {
+            child.visibility = View.INVISIBLE
+        } else if (child.visibility != View.VISIBLE && dyConsumed < 0) {
+            child.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type)
+    }
+}
